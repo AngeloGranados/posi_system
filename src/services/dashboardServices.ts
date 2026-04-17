fdc6ddf06674a3ac01a3514e4cb5121793972c96
AngeloGranados <angelo_16.1@hotmail.com>
2026-04-17 17:27:16 -0500

    fix: agregar tipo de dato en el registro de datos recibidos en getDataCompany

--- src/services/dashboardServices.ts
+++ src/services/dashboardServices.ts
@@ -41,7 +41,7 @@ export async function getDataCompany(): Promise<CompanyData> {
         throw new Error("Failed to fetch company data");
     }
 
-    console.log("Data received from API:", data);
+    console.log(typeof data, "Data received from API:", data);
 
     if (typeof data === "string") {
         data = JSON.parse(data);
