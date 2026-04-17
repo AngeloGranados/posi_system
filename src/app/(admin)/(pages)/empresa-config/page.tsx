fd587ffb74aad99847c93697eb79a8b7c1f7da75
AngeloGranados <angelo_16.1@hotmail.com>
2026-04-17 17:29:11 -0500

    fix: agregar registro de datos de la compañía en EmpresaConfig

--- src/app/(admin)/(pages)/empresa-config/page.tsx
+++ src/app/(admin)/(pages)/empresa-config/page.tsx
@@ -5,6 +5,8 @@ export default async function EmpresaConfig() {
 
     const dataCompany: CompanyData = await getDataCompany();
 
+    console.log("Data Company in EmpresaConfig:", dataCompany);
+
     return (
         <div className="pb-20">
             <ConfigModal dataCompany={dataCompany} />
